24a5fbad17fc9eed753e8fc312f7875f5bfafd99
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-15 11:46:42 -0500

    Edits to methods for print

--- main.py
+++ main.py
@@ -6,14 +6,5 @@ jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
 sonny.greet(jordan)
 jordan.greet(sonny)
 
-print(f'''
-Here is {sonny.name}'s contact info:
-Phone number: {sonny.phone}
-Email Address: {sonny.email}
-''')
-
-print(f'''
-Here is {jordan.name}'s contact info:
-Phone number: {jordan.phone}
-Email Address: {jordan.email}
-''')
+sonny.print_phone_email()
+jordan.print_phone_email()
