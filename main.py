2bdfd02c8f6d2263be6330a59505706d7d1f0b00
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-15 12:44:19 -0500

    Bonus Challenge and str implementation

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ from vehicle import Vehicle
 
 sonny = Person("Sonny", "sonny@hotmail.com", "483-485-4948")
 jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
+betty = Person("Betty", 'b', '4')
+eddie = Person("Eddie", "d", "5")
 
+
+# sonny.greet(jordan)
+# jordan.greet(sonny)
+
+# sonny.print_contact_info()
+# jordan.print_contact_info()
+
+# car = Vehicle("Nissan", "Leaf", 2015)
+# truck = Vehicle("Ford", "F-150", 2002)
+# sport = Vehicle("Audi", "R8", 2021)
+
+# car.print_info()
+
+# jordan.add_friends("Harry")
+# jordan.add_friends("Ed")
+# jordan.add_friends("Carl")
+
+# jordan.num_friends()
+# print(jordan.greeting_count)
+# print(jordan.friends)
+
+sonny.greet(betty)
+print(sonny.num_unique_people_greeted)
+sonny.greet(eddie)
+print(sonny.num_unique_people_greeted)
+sonny.greet(eddie)
+print(sonny.num_unique_people_greeted)
 sonny.greet(jordan)
-jordan.greet(sonny)
-
-sonny.print_contact_info()
-jordan.print_contact_info()
-
-car = Vehicle("Nissan", "Leaf", 2015)
-truck = Vehicle("Ford", "F-150", 2002)
-sport = Vehicle("Audi", "R8", 2021)
-
-car.print_info()
-
-jordan.add_friends("Harry")
-jordan.add_friends("Ed")
-jordan.add_friends("Carl")
-
-jordan.num_friends()
-print(jordan.greeting_count)
-print(jordan.friends)
+print(sonny.num_unique_people_greeted)
--- person.py
+++ person.py
@@ -5,10 +5,19 @@ class Person:
         self.phone = phone
         self.friends = []
         self.greeting_count = 0
+        self.num_unique_people_greeted = 0
+        self.people_greeted = []
 
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person.name, self.name))
         self.greeting_count += 1
+        new_person = True
+        for each in self.people_greeted:
+            if other_person.name == each:
+                new_person = False
+        if new_person:
+            self.people_greeted.append(other_person.name)
+            self.num_unique_people_greeted += 1
 
     def print_contact_info(self):
         print(f'''
@@ -23,3 +32,6 @@ Here is {self.name}'s contact info:
 
     def num_friends(self):
         print(len(self.friends))
+
+    def __str__(self):
+        return f'Person: {self.name} {self.email} {self.phone}'
