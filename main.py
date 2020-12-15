e638e7ad797d515f8dc468403831b09d8a63e694
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2020-12-15 12:22:23 -0500

    added methods to Person class. Part three of the exercises

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
 sonny.greet(jordan)
 jordan.greet(sonny)
 
-sonny.print_phone_email()
-jordan.print_phone_email()
+sonny.print_contact_info()
+jordan.print_contact_info()
 
 car = Vehicle("Nissan", "Leaf", 2015)
+truck = Vehicle("Ford", "F-150", 2002)
+sport = Vehicle("Audi", "R8", 2021)
 
 car.print_info()
--- person.py
+++ person.py
@@ -3,13 +3,23 @@ class Person:
         self.name = name
         self.email = email
         self.phone = phone
+        self.friends = []
+        self.greeting_count = []
 
     def greet(self, other_person):
         print('Hello {}, I am {}!'.format(other_person.name, self.name))
+        self.greeting_count += 1
 
-    def print_phone_email(self):
+    def print_contact_info(self):
         print(f'''
 Here is {self.name}'s contact info:
-Phone number: {self.phone}
-Email Address: {self.email}
+
+{self.name}'s email: {self.email}
+{self.name}'s phone number: {self.phone}
 ''')
+
+    def add_friends(self, friend):
+        self.friends.append(friend)
+
+    def num_friends(self):
+        print(len(self.friends))
